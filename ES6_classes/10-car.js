d84aca568d961d0f7ebdaf8eb11d055cad8d2ec6
Sandya1988 <Aoun.sandy@gmail.com>
2024-08-26 23:27:25 +0300

    dd

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -4,11 +4,9 @@ export default class Car {
       this._motor = motor;
       this._color = color;
     }
-  
     static get [Symbol.species]() {
       return this.prototype.constructor;
     }
-  
     cloneCar() {
       return new (this.constructor[Symbol.species])();
     }
